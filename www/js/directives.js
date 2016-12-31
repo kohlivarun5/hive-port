dd8d761889f06e573e0fd081dd6148c5c98a1fbd
Varun Kohli <kohli_varun5@yahoo.com>
2016-12-31 16:32:16 -0500

    new summary

--- www/js/directives.js
+++ www/js/directives.js
@@ -5,14 +5,25 @@ angular.module('starter.directives', [])
     scope: {
       ticker: "=",
       quantity: "=",
-      total_cost: "="
+      cost: "="
     },
     link: function(scope) {
       StockPrices.get(scope.ticker,function(price,change) {
-        scope.price=price;
-        scope.change=change;
+        scope.price=parseFloat(price);
+        scope.change=parseFloat(change);
+        scope.net=(scope.quantity*scope.price)-scope.cost;
         console.log(scope);
       });
+    }
+  };
+})
+.directive("amount", function(StockPrices) {
+  return {
+    templateUrl:"templates/directive-amount.html",
+    scope: {
+      value: "=",
+      pre: "=",
+      post: "=",
     }
   };
 })
